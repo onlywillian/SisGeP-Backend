d1654ff3a2346a0593824e7c71a258192e5aef00
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-09-25 16:05:31 -0300

    database configs

---
+++ src/database/config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsT_XjScSdqeqm92IWhjBYQaSBSd5whxk",
+  authDomain: "sisgep-7bc34.firebaseapp.com",
+  projectId: "sisgep-7bc34",
+  storageBucket: "sisgep-7bc34.appspot.com",
+  messagingSenderId: "35711298085",
+  appId: "1:35711298085:web:29e6378acd1aa52206edcc"
+};
+
+// Initialize Firebase  
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default db;
