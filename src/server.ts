5aa856c7bfd10faa90ab3acd4ac0ec91745b688a
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-09-25 16:09:39 -0300

    server file and the rest of files

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import cors from "cors";
+
+import usersRouter from './routes/users/route';
+import locationsRouter from './routes/spaces/route';
+import equipmentsRouter from './routes/equipments/route';
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// rotas 
+app.use(usersRouter);
+app.use(locationsRouter);
+app.use(equipmentsRouter);
+
+app.listen(3001, () => {console.log("running on port http://localhost:3001")})
