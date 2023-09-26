53a84061ac17fc6e4393a29e4c4b2f0005fa5f71
onlywillian <willianandrade953@gmail.com>
2023-09-26 16:19:37 -0300

    dependencies updates

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
-import express from 'express';
+import express from "express";
 
 import cors from "cors";
 
-import usersRouter from './routes/users/route';
-import locationsRouter from './routes/spaces/route';
-import equipmentsRouter from './routes/equipments/route';
+import usersRouter from "./routes/users/route";
+import locationsRouter from "./routes/locations/route";
+import equipmentsRouter from "./routes/equipments/route";
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-// rotas 
+// rotas
 app.use(usersRouter);
 app.use(locationsRouter);
 app.use(equipmentsRouter);
 
-app.listen(3001, () => {console.log("running on port http://localhost:3001")})
+app.listen(3001, () => {
+  console.log("running on port http://localhost:3001");
+});
