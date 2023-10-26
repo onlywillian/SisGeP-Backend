8cfea3cab56da484ee09cd68ee03a3546db207d4
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-10-26 09:51:09 -0300

    changingRouteOfQrCode

--- src/services/createQRcode.ts
+++ src/services/createQRcode.ts
@@ -2,6 +2,6 @@ import QRCode from "qrcode";
 
 type values =  "equipments"| "locations"|"users"
 
-export default async function createQRcode(ref: number | string, type: values) {
-    return await QRCode.toDataURL(`https://sisgep-api.onrender.com/${type}/${ref}`, { scale: 8 })
+export default async function createQRcode(ref: number, type: values) {
+    return await QRCode.toDataURL(`${ref}`, { scale: 8 })
 }
